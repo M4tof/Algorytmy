7b1a08aa2687a5b1f29d4031139ee6bc76fa68b8
KrzysManPc <krzysztfmanczak@gmail.com>
2023-03-04 10:53:36 +0100

    runner

---
+++ Runner.py
@@ -0,0 +1,11 @@
+import time
+#import algorithm
+
+startTime = time.time() #time at start
+
+print('nic'*200000)
+
+
+endTime= time.time()    #time at end
+totalTime= endTime - startTime  #run time
+print(totalTime)
