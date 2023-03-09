b3dbde90ea1bddeab712dfe89d5cdfe662d154cb
M4tof <121232863+M4tof@users.noreply.github.com>
2023-03-09 17:59:07 +0100

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -1,17 +1,23 @@
 import time
 import Sorting
 import RandNum
+from time import sleep
+from threading import Timer
 
 tab=RandNum.RandGen()
 
 startTime = time.time() #time at start
 #############################################CODE BLOCK####################
 
-Sorting.mergeSort(tab)
+#tab=Sorting.sleepSort(tab)
+#Sorting.insertSort(tab)
+#Sorting.selectionSort(tab)
+#Sorting.mergeSort(tab)
+#Sorting.quickSort(tab,0,(len(tab)-1))
 
 print(tab)
 
 #############################################CODE BLOCK####################
 endTime= time.time()    #time at end
 totalTime= endTime - startTime  #run time
-print('Total run time =',totalTime)    #display run time
+print('Total run time =',(totalTime))    #display run time
